88dc358ef9ec6c1809d2937289932e13f7fdb772
Amir ali <amiralikhamseh01@gmail.com>
2025-03-12 22:42:21 +0100

    feature :Grid  layout implemented and Popular products componented created

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CardPopularProducts from "./CardPopularProducts";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+    </div>
+  );
 };
 
 export default Dashboard;
